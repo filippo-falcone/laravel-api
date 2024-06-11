f95eda8c6023a7a3f5b482f20726de64d97d99dc
filippo-falcone <falconefilippo98@hotmail.com>
2024-06-11 17:56:06 +0000

    feat: add show function to api projectsController and creat relative route

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -17,4 +17,21 @@ class ProjectController extends Controller
         ];
         return response()->json($data);
     }
+
+    public function show($slug)
+    {
+        $project = Project::where('slug', '=', $slug)->with('type', 'technologies')->first();
+        if ($project) {
+            $data = [
+                'success' => true,
+                'projects' => $project
+            ];
+        } else {
+            $data = [
+                'success' => false,
+                'error' => 'Project not found'
+            ];
+        }
+        return response()->json($data);
+    }
 }
